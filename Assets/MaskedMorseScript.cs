155be9fbd65ebebb0ebbb87edb91c41e34a8021d
Victor Louie <58094419+vitzlo@users.noreply.github.com>
2021-08-31 16:11:09 -0400

    Formal release
    yeah

--- Assets/MaskedMorseScript.cs
+++ Assets/MaskedMorseScript.cs
@@ -18,7 +18,6 @@ public class MaskedMorseScript : MonoBehaviour {
     public MeshRenderer[] gridColors, answerColors;
     public GameObject speedBase;
     public GameObject[] answerBases = new GameObject[3];
-    public Coroutine coroutine;
 
     private readonly Dictionary<char, string> morse = new Dictionary<char, string> {
         {'A', ".-"}, {'B', "-..."}, {'C', "-.-."}, {'D', "-.."}, {'E', "."}, {'F', "..-."},
@@ -30,6 +29,7 @@ public class MaskedMorseScript : MonoBehaviour {
     };
 
     private const string table = "LS3QJO2BKN49TMRAPIC1EX85FUZ6HW0DGVY7";
+    private const string headers = "ABCDEF123456";
 
     private List<List<int>> lines = new List<List<int>>() {
             new List<int>() {0, 1, 2, 3, 4, 5}, new List<int>() {6, 7, 8, 9, 10, 11},
@@ -51,7 +51,8 @@ public class MaskedMorseScript : MonoBehaviour {
     private List<List<bool>> marqueeLights = new List<List<bool>>(); // where the colors are along their morse transmissions
     private int[] correctLine = new int[3]; // the "read"
     private List<int> correctPresses = new List<int>(); // the "answer"
-    private List<bool> pressesList = new List<bool>() {false, false, false};
+    private List<bool> pressesList = new List<bool>() {false, false, false}; // which answer squares have been pressed
+    private Coroutine coroutine;
 
     static int moduleIdCounter = 1;
     int moduleId;
@@ -112,12 +113,54 @@ public class MaskedMorseScript : MonoBehaviour {
         Debug.LogFormat("[Marquis Morse #{0}] Module solved.", moduleId);
         Module.HandlePass();
         speed = 0;
-        StopCoroutine(coroutine);
-        ClearGrid();
+        if (coroutine != null) StopCoroutine(coroutine);
+        for (int i = 0; i < 3; i++) answerColors[i].material.color = Color.black;
         foreach (MeshRenderer mat in answerColors) mat.material.color = Color.green;
         StartCoroutine(MoveSpeedBase(false));
+        StartCoroutine(AnimateAnswers());
+        StartCoroutine(AnimateGrid());
     }
 
+    // animates the answer screens upon a solve
+    private IEnumerator AnimateAnswers() {
+        List<float> waitList = new List<float> {0.2f, 0.65f, 0.65f, 0.25f, 0.25f, 0.25f, 0.25f, 0.25f, 0.25f};
+        yield return new WaitForSeconds(1.2f);
+        for (int i = 0; i < 9; i++) {
+            answerColors[i % 3].material.color = i >= 3 && i < 6 ? Color.green : Color.black;
+            yield return new WaitForSeconds(waitList[i]);
+        }
+        yield return null;
+    }
+
+    // animates the grid colors upon a solve
+    private IEnumerator AnimateGrid() {
+        List<int> flashingCells;
+        if (UnityEngine.Random.Range(0, 100) == 51) {
+            flashingCells = new List<int>
+                {0, 1, 2, 3, 4, 6, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 30, 34}; // uh oh!
+        }
+        else {
+            flashingCells = new List<int> {5, 10, 11, 15, 16, 18, 20, 21, 24, 25, 26, 31}; // checkmark
+        }
+
+        List<int> clearOrder = new List<int>();
+        for (int i = 0; i < 36; i++) {
+            if (gridColors[i].material.color != Color.black) clearOrder.Add(i);
+        }
+        clearOrder.Shuffle();
+        foreach (int pos in clearOrder) {
+            gridColors[pos].material.color = Color.black;
+            yield return new WaitForSeconds(2.5f / clearOrder.Count);
+        }
+        for (int i = 0; i < 6; i++) {
+            foreach (int pos in flashingCells) {
+                gridColors[pos].material.color = i % 2 == 0 ? Color.green : Color.black;
+            }
+            yield return new WaitForSeconds(0.2f);
+        }
+    }   
+
+    // manages the updating of the screen's colors
     private IEnumerator Tick() {
         while (speed != 0) {
             UpdateGrid();
@@ -126,6 +169,7 @@ public class MaskedMorseScript : MonoBehaviour {
         yield return null;
     }
 
+    // shifts the pointer in the morse transmissions and updates the grid colors accordingly
     private void UpdateGrid() {
         for (int i = 0; i < 36; i++) newColors[i] = Color.black;
         for (int i = 0; i < 3; i++) {
@@ -141,10 +185,13 @@ public class MaskedMorseScript : MonoBehaviour {
         for (int i = 0; i < 3; i++) answerColors[i].material.color = newColors[correctPresses[i]];
     }
 
+    // sets every color to black
     private void ClearGrid() {
         for (int i = 0; i < 36; i++) gridColors[i].material.color = Color.black;
+        for (int i = 0; i < 3; i++) answerColors[i].material.color = Color.black;
     }
 
+    // converts the given character to a list of booleans representing the character in Morse
     private List<bool> MorseSquares(char c) {
         List<bool> result = new List<bool>();
         foreach (char m in morse[c]) {
@@ -154,14 +201,15 @@ public class MaskedMorseScript : MonoBehaviour {
         return result;
     }
 
+    // applies the module's offset to the given position in the grid
     private int Step(int pos) {
         pos += pos % 6 + offsetX >= 6 ? offsetX - 6 : offsetX;
         pos += pos / 6 + offsetY >= 6 ? 6 * (offsetY - 6) : 6 * offsetY;
         return pos;
     }
 
+    // converts the given position to Excel coordinates
     private string ToCoordinate(int pos) {
-        String headers = "ABCDEF123456";
         return "" + headers[pos % 6] + headers[pos / 6 + 6];
     }
 
@@ -171,6 +219,7 @@ public class MaskedMorseScript : MonoBehaviour {
         return rgb[rgb.IndexOf(current) + (int) Math.Pow(2, 2 - add)];
     }
 
+    // returns whether the given integers share no factors other than 1
     private bool Coprime(int a, int b) {
         for (int i = 2; i <= Math.Min(a, b); i++) if (a % i == 0 && b % i == 0) return false;
         return true;
@@ -184,6 +233,7 @@ public class MaskedMorseScript : MonoBehaviour {
                 Debug.LogFormat("[Marquis Morse #{0}] {1} was pressed. That was correct.", moduleId, ToCoordinate(i));
                 StartCoroutine(MoveAnswerBase(correctPresses.IndexOf(i)));
                 pressesList[correctPresses.IndexOf(i)] = true;
+                Audio.PlaySoundAtTransform("correct" + pressesList.Count(x => x), Module.transform);
                 if (pressesList.All(x => x)) Solve();
             }
             else if (correctPresses.Contains(i)) {
@@ -201,6 +251,7 @@ public class MaskedMorseScript : MonoBehaviour {
     private KMSelectable.OnInteractHandler SpeedPress(bool up) {
         return delegate {
             (up ? speedButtonUp : speedButtonDown).AddInteractionPunch(.1f);
+            Audio.PlayGameSoundAtTransform(KMSoundOverride.SoundEffect.ButtonPress, (up ? speedButtonUp : speedButtonDown).transform);
             speed = up ? Math.Min(5, speed + 1) : Math.Max(0, speed - 1);
             speedText.text = speed.ToString();
             if (speed == 0) {
@@ -212,6 +263,7 @@ public class MaskedMorseScript : MonoBehaviour {
         };
     }
 
+    // moves the speed panel in or out, according to the given state
     private IEnumerator MoveSpeedBase(bool slideOut) {
         float from = speedBase.transform.localPosition.x;
         float to = from + (slideOut ? -19.0f : 19.0f);
@@ -225,9 +277,10 @@ public class MaskedMorseScript : MonoBehaviour {
         speedBase.transform.localPosition = new Vector3(to, speedBase.transform.localPosition.y, speedBase.transform.localPosition.z);
     }
     
+    // moves the answer base at the given position outward
     private IEnumerator MoveAnswerBase(int index) {
         float from = answerBases[index].transform.localPosition.y;
-        float to = from + 14.0f;
+        float to = from + 13.8f;
         float startTime = Time.fixedTime;
         float duration = 1.5f;
         do {
@@ -239,18 +292,32 @@ public class MaskedMorseScript : MonoBehaviour {
     }
 
     #pragma warning disable 414
-    private readonly string TwitchHelpMessage = @"Use <!{0} foobar> to do something.";
+    private readonly string TwitchHelpMessage = @"Use <!{0} speed #> to set the ticking speed. Use <!{0} A2> to press the cell in the first column and second row. Grid presses can be chained with spaces.";
     #pragma warning restore 414
 
-    IEnumerator ProcessTwitchCommand (string command)
-    {
+    IEnumerator ProcessTwitchCommand (string command) {
         command = command.Trim().ToUpperInvariant();
         List<string> parameters = command.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).ToList();
+
+        int newSpeed;
+        if (parameters.Count >= 4) yield return "sendtochaterror Too many commands.";
+        if (parameters.Count == 2 && parameters[0] == "SPEED" && Int32.TryParse(parameters[1], out newSpeed) && newSpeed >= 0 && newSpeed < 6) {
+            int loopTime = Math.Abs(newSpeed - speed);
+            for (int i = 0; i < loopTime; i++) {
+                Debug.Log("speed = " + speed);
+                Debug.Log("new speed = "+ newSpeed);
+                (newSpeed > speed ? speedButtonUp : speedButtonDown).OnInteract();
+            }
+        }
+        else if (parameters.All(s =>
+            s.Length == 2 && headers.Substring(0, 6).Contains(s[0]) && headers.Substring(6, 6).Contains(s[1]))) {
+            foreach (string s in parameters) gridButtons[headers.IndexOf(s[0]) + 6 * (headers.IndexOf(s[1]) - 6)].OnInteract();
+        }
+        else yield return "sendtochaterror Invalid command.";
         yield return null;
     }
 
-    IEnumerator TwitchHandleForcedSolve ()
-    {
+    IEnumerator TwitchHandleForcedSolve () {
         yield return null;
     }
 }
